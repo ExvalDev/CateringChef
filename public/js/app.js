0591a0b7e16ee1deb204fe1e58009907faf4b86a
chriskch <chriskoch170@gmail.com>
2019-12-05 12:32:27 +0100

    ...

--- public/js/app.js
+++ public/js/app.js
@@ -49762,9 +49762,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\CateringChef\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\xampp\htdocs\CateringChef\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\CateringChef\resources\sass\style.scss */"./resources/sass/style.scss");
+__webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/cateringchef/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/cateringchef/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/cateringchef/resources/sass/style.scss */"./resources/sass/style.scss");
 
 
 /***/ })
