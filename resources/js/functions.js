96c1c5baa28a3c74c8d052cbfb932fd42e8d3cd5
ZeevBenni <benni.lamm@gmail.com>
2020-01-24 21:29:15 +0100

    compact funtions.js search & redisgn add component

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -1,15 +1,18 @@
-//Search Ingredient
-$(document).ready(function()
-{
-    $("#SearchIngredient").on("keyup", function() 
+
+/**
+ *Search in a List by input
+ * @param {String} inputField - id of the Field where search input ('#id')
+ * @param {String} listName - id of ul where the span will be searched for ('id')
+ */
+function searchInUl(inputField, listName){
+    $(inputField).on("keyup", function() 
     {
         // Declare variables
         var filter, ul, li, span, i, txtValue;
         filter = $(this).val().toLowerCase();
-        ul = document.getElementById("ListIngredient");
+        ul = document.getElementById(listName);
         li = ul.getElementsByTagName('li');
         
-
         // Loop through all list items, and hide those who don't match the search query
         for (i = 0; i < li.length; i++) 
         {
@@ -25,75 +28,20 @@ $(document).ready(function()
             }
         }
     });
-});
+}
 
-//Search Component
-$(document).ready(function()
-{
-    $("#SearchComponent").on("keyup", function() 
-    {
-        // Declare variables
-        var filter, ul, li, span, i, txtValue;
-        filter = $(this).val().toLowerCase();
-        ul = document.getElementById("ListComponent");
-        li = ul.getElementsByTagName('li');
-        
-
-        // Loop through all list items, and hide those who don't match the search query
-        for (i = 0; i < li.length; i++) 
-        {
-            span = li[i].getElementsByTagName("span")[0];
-            txtValue = span.innerText;
-            if (txtValue.toLowerCase().indexOf(filter) > -1) 
-            {
-                li[i].style.display = "";
-            } 
-            else 
-            {
-                li[i].setAttribute('style', 'display: none !important');
-            }
-        }
-    });
-});
-
-//Search Meal
-$(document).ready(function()
-{
-    $("#SearchMeal").on("keyup", function() 
-    {
-        // Declare variables
-        var filter, ul, li, span, i, txtValue;
-        filter = $(this).val().toLowerCase();
-        ul = document.getElementById("ListMeal");
-        li = ul.getElementsByTagName('li');
-        
-
-        // Loop through all list items, and hide those who don't match the search query
-        for (i = 0; i < li.length; i++) 
-        {
-            span = li[i].getElementsByTagName("span")[0];
-            txtValue = span.innerText;
-            if (txtValue.toLowerCase().indexOf(filter) > -1) 
-            {
-                li[i].style.display = "";
-            } 
-            else 
-            {
-                li[i].setAttribute('style', 'display: none !important');
-            }
-        }
-    });
-});
-
-//Search Customers
-$(document).ready(function()
-{
-    $("#SearchCustomer").on("keyup", function() 
+/**
+ * Search in a table by input
+ * @param {String} inputField - id of the Field where search input ('#id')
+ * @param {String} listName - id of tablebody where the text will be searched for ('id')
+ */
+function searchInTable(inputField, tableName){
+    $(inputField).on("keyup", function() 
     {
         // Declare variables
         var filter, tableBody, tRow, Item, i, txtValue;
         filter = $(this).val().toLowerCase();
-        tableBody = document.getElementById("TableCustomer");
+        tableBody = document.getElementById(tableName);
         tRow = tableBody.getElementsByTagName('tr');
         
         // Loop through all list items, and hide those who don't match the search query
@@ -111,35 +59,29 @@ $(document).ready(function()
             }
         }
     });
-});
+}
 
-//Search Customers
+// Search on Table View
 $(document).ready(function()
 {
-    $("#SearchSupplier").on("keyup", function() 
-    {
-        // Declare variables
-        var filter, tableBody, tRow, Item, i, txtValue;
-        filter = $(this).val().toLowerCase();
-        tableBody = document.getElementById("TableSupplier");
-        tRow = tableBody.getElementsByTagName('tr');
-        
-        // Loop through all list items, and hide those who don't match the search query
-        for (i = 0; i < tRow.length; i++) 
-        {
-            Item = tRow[i].getElementsByClassName('searchItem')[0];
-            txtValue = Item.innerText;
-            if (txtValue.toLowerCase().indexOf(filter) > -1) 
-            {
-                tRow[i].style.display = "";
-            } 
-            else 
-            {
-                tRow[i].setAttribute('style', 'display: none !important');
-            }
-        }
-    });
+    //Search Ingredient
+    searchInUl("#SearchIngredient","ListIngredient");
+    //Search Component
+    searchInUl("#SearchComponent","ListComponent");
+    //Search Meal
+    searchInUl("#SearchMeal","ListMeal");
+    
 });
+
+// Search on Customer & Suppliers View
+$(document).ready(function()
+{
+    //Search Customers
+    searchInTable("#SearchCustomer", "TableCustomer");
+    //Search Suppliers
+    searchInTable("#SearchSupplier", "TableSupplier");
+});
+
 
 //Show Ingredient
 $(document).ready(function(){
@@ -195,14 +137,14 @@ $('.add-one').click(function(){
     $('.selectIngredient').last().attr('id', 'selectIngredient'+count);
     $('.selectIngredient').last().attr('onchange', 'changeUnit('+count+')');
     $('.unitIngredient').last().attr('id', 'unitIngredient'+count);
-  });//Clone the hidden element and shows it
+});//Clone the hidden element and shows it
   
-  function attach_delete(){
+function attach_delete(){
     $('.delete').off();
     $('.delete').click(function(){
-      $(this).closest('.form-group').remove();
+        $(this).closest('.dynamic-element').remove();
     });
-  }//Attach functionality to delete buttons
+}//Attach functionality to delete buttons
 
 //Show Component
 $(document).ready(function(){
@@ -255,15 +197,17 @@ $(document).ready(function(){
 		current_step = $(this).parent();
 		next_step = $(this).parent().next();
 		next_step.show();
-		current_step.hide();
-		setProgressBar(++current);
+        current_step.hide();
+        //activate next step on progressbar using the index of next_fs
+        $("#progressbar li").eq($("fieldset").index(next_step)).addClass("active");
 	});
 	$(".previous").click(function(){
 		current_step = $(this).parent();
 		next_step = $(this).parent().prev();
 		next_step.show();
 		current_step.hide();
-		setProgressBar(--current);
+		//de-activate current step on progressbar
+        $("#progressbar li").eq($("fieldset").index(current_step)).removeClass("active");
 	});
 	setProgressBar(current);
 	// Change progress bar action
