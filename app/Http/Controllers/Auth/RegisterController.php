3689ba4cd7274d76b3ad6301a6c4b7e9d7d10768
ZeevBenni <benni.lamm@gmail.com>
2020-02-04 10:13:38 +0100

    changed register route

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,11 +64,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        User::create([
             'firstname' => $data['firstname'],
             'surname' => $data['surname'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        return redirect('/login');
     }
 }
