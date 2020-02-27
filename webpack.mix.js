c28b3ae11a272d68b8f77f10204dfaff76c3febe
ZeevBenni <benni.lamm@gmail.com>
2020-02-27 14:20:52 +0100

    video

--- webpack.mix.js
+++ webpack.mix.js
@@ -22,6 +22,7 @@ mix.js('resources/js/tables.js', 'public/js')
    .sass('resources/sass/style_menu.scss', 'public/css')
    .sass('resources/sass/style_checkbox.scss', 'public/css')
 mix.copyDirectory('resources/img', 'public/img');
+mix.copyDirectory('resources/videos', 'public/videos');
 mix.copyDirectory('resources/favicon', 'public/favicon');
 mix.copy('resources/js/menu.js', 'public/js');
 mix.copy('resources/fonts/kreon.z', 'vendor/tecnickcom/tcpdf/fonts');
