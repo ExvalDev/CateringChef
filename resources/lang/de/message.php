f7a4be3abe000b14cc5cdac71a297d20fab43453
mirakulix97 <michaeljall@web.de>
2020-02-29 15:05:56 +0100

    Seite sollte nicht mehr offline gehen, Tooltip Anmerkung unter der Karte

--- resources/lang/de/message.php
+++ resources/lang/de/message.php
@@ -60,11 +60,13 @@ return [
     'titleMeal' => 'Bedienungshilfe Speisen',
     'infoMeal' => 'Die Speisen sind die wichtigsten Bestandteile des Systems. Zusammengesetzt aus verschiedenen Komponenten kann man sie im Speiseplan zur Planung verwenden.',
     'titleSupplier' => 'Bedienungshilfe Lieferanten',
-    'infoSupplier' => 'Hier werden die einzelnen Lieferanten erfasst. Angeben wird die Adresse, welche dann später in der Karte eingesehen werden kann.',
+    'infoSupplier' => 'Hier werden die einzelnen Lieferanten erfasst. Angeben wird die Adresse, welche dann später in der Karte eingesehen werden kann. <br> Das ist ein Test',
     'titleCustomer' => "Bedienungshilfe Kunden",
     'infoCustomer' => 'Hier werden die einzelnen Kunden erfasst. Angegeben werden die Anzahl der Erwachsenen und Kinder und die Standorte der Kunden, welche dann in der Karte eingesehen werden können.',
     'titleMenu' => 'Bedienungshilfe Speiseplan',
     'infoMenu' => 'Sie können alle Speisen per Drag & Drop aus der Speiseliste in den gewünschten Tag im Speiseplan ziehen. Durch das Ziehen einer Speise über den Löschen-Bereich kann man eine bereits im Plan angelegte Speise wieder entfernen.',
     'titleShoppingList' => 'Bedienungshilfe Einkaufsliste',
-    'infoShoppingList' => 'Hiermit wird die Einkaufsliste erstellt. Ausgewählt werden je erfasster Kunde die Anzahl an Erwachsenen und Kindern und ein Zeitraum, für den die Einkaufsliste - geordnet nach Lieferanten - erstellt wird.'
+    'infoShoppingList' => 'Hiermit wird die Einkaufsliste erstellt. Ausgewählt werden je erfasster Kunde die Anzahl an Erwachsenen und Kindern und ein Zeitraum, für den die Einkaufsliste - geordnet nach Lieferanten - erstellt wird.',
+    'titleMap' => 'Anmerkung Karte',
+    'infoMap' => 'Falls nicht alle Markierungen in der Karte angezeigt werden: Bitte überprüfen Sie die eingetragenen Adressen'
 ];
